6687f4b1bfca2b052337fddf1dfeaee921eacd98
JPBM135 <jpedrobm0@gmail.com>
2024-06-25 03:29:07 -0700

    feat(client): sentry and prod api

--- packages/client/src/environment/environment.ts
+++ packages/client/src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   isProduction: true,
-  sentryDSN: '',
-  apiUrl: 'http://localhost:4000/graphql',
+  sentryDSN: 'https://33a6227ace100d5d305625853d79e3a9@o4505281572765696.ingest.us.sentry.io/4507492104667136',
+  apiUrl: 'https://api.jpbm.dev/job-hub/graphql',
 };
