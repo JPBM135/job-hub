402f356bed31a14dbd9cf8c32f718db423dd0555
JPBM135 <jpedrobm0@gmail.com>
2024-07-05 11:46:02 -0700

    feat(meta): github actions

--- packages/client/src/app/core/animations/heightGrow.ts
+++ packages/client/src/app/core/animations/heightGrow.ts
@@ -1,8 +1,5 @@
 import { trigger, style, transition, animate } from '@angular/animations';
 
 export const heightGrow = trigger('heightGrow', [
-  transition(':enter', [
-    style({ height: '0' }),
-    animate('0.2s ease-in-out', style({ height: '*' })),
-  ]),
+  transition(':enter', [style({ height: '0' }), animate('0.2s ease-in-out', style({ height: '*' }))]),
 ]);
--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -1,8 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="@angular/localize" />
 
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((error) => console.error(error));
